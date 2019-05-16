b77336a59cd719e9b5a8f858299f55f7b83ab665
xzhuo <yunkzo@gmail.com>
2019-05-16 12:18:14 -0500

    bat genomes

--- frontend/src/model/genomes/allGenomes.ts
+++ frontend/src/model/genomes/allGenomes.ts
@@ -1,27 +1,21 @@
-import HG19 from './hg19/hg19';
 import HG38 from './hg38/hg38';
 import MM10 from './mm10/mm10';
-import DAN_RER10 from './danRer10/danRer10';
 import PANTRO5 from './panTro5/panTro5';
 import RN6 from './rn6/rn6';
-import MM9 from './mm9/mm9';
-import BosTau8 from './bosTau8/bosTau8';
-import AraTha1 from './araTha1/araTha1';
+import MYOLUC2 from './myoLuc2/myoLuc2';
+import PTEVAM1 from './pteVam1/pteVam1';
 import { GenomeConfig } from './GenomeConfig';
 
 /**
  * All available genomes.
  */
 export const allGenomes = [
-    HG19,
     HG38,
     MM10,
-    MM9,
     PANTRO5,
-    BosTau8,
-    DAN_RER10,
     RN6,
-    AraTha1,
+    MYOLUC2,
+    PTEVAM1
 ];
 
 const genomeNameToConfig = {};
@@ -56,26 +50,22 @@ export const treeOfLife: {[speciesName: string]: SpeciesConfig} = {
         assemblies: [ MM10.genome.getName(), MM9.genome.getName() ],
         color: 'yellow',
     },
-    cow: {
-        logoUrl: 'https://vizhub.wustl.edu/public/bosTau8/Cow.png',
-        assemblies: [ BosTau8.genome.getName() ],
-        color: 'yellow',
-    },
     rat: {
         logoUrl: 'https://epigenomegateway.wustl.edu/browser/images/Rat.png',
         assemblies: [ RN6.genome.getName() ],
         color: 'white',
     },
-    zebrafish: {
-        logoUrl: 'https://epigenomegateway.wustl.edu/browser/images/Zebrafish.png',
-        assemblies: [ DAN_RER10.genome.getName() ],
-        color: 'yellow',
+    "large flying fox": {
+        logoUrl: 'https://wangftp.wustl.edu/~xzhuo/bat_genomes/flyingFox.png',
+        assemblies: [ PTEVAM1.genome.getName() ],
+        color: 'white',
     },
-    arabidopsis: {
-        logoUrl: 'https://epigenomegateway.wustl.edu/browser/images/Arabidopsis.png',
-        assemblies: [ AraTha1.genome.getName() ],
-        color: 'yellow',
+    "little brown bat": {
+        logoUrl: 'https://wangftp.wustl.edu/~xzhuo/bat_genomes/littleBrownBat.png',
+        assemblies: [ MYOLUC2.genome.getName() ],
+        color: 'white',
     },
+
 };
 
 /**
